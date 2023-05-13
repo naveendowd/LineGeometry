aeb76ab31859b568289da66524a8cb88617b62ab
naveendowd <119478972+naveendowd@users.noreply.github.com>
2023-05-13 13:03:40 +0530

    Welcome to LineGeometry

---
+++ GeometryLiness/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryLines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Geometry Co-ordinates");
+            Geometryco_ordinates.Coordinates();
+        }
+    }
+}
